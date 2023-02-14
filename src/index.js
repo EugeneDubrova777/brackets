242629212da657ca6c28a4e6db5158196dc95c3c
EugeneDubrova777 <blitzkriegbop777@gmail.com>
2023-02-14 11:04:44 +0300

    feat: solve task

--- src/index.js
+++ src/index.js
@@ -1,3 +1,16 @@
 module.exports = function check(str, bracketsConfig) {
-  // your solution
+  const brackets = [];
+
+  if (str.length % 2 > 0) {
+    return false;
+  }
+  for (let i = 0; i < str.length; i++) {
+    for (let j = 0; j < bracketsConfig.length; j++) {
+      if (str[i] === bracketsConfig[j][1] && brackets[brackets.length - 1] === bracketsConfig[j][0]) brackets.pop();
+      else if (str[i] === bracketsConfig[j][0]) brackets.push(str[i]);      
+    }
+  }
+
+  if (brackets.length === 0) return true;
+  else return false;
 }
